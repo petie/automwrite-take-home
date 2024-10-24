0038bf1cca4132b84b37b551d61e6bddea28f2ba
7obyGit <7obyGitt@gmail.com>
2024-10-24 14:36:13 +0100

    Initial commit

---
+++ src/main/java/com/automwrite/assessment/recruitmentseniortakehome/RecruitmentSeniorTakeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.automwrite.assessment.recruitmentseniortakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentSeniorTakeHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentSeniorTakeHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/automwrite/assessment/recruitmentseniortakehome/RecruitmentSeniorTakeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automwrite.assessment.recruitmentseniortakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentSeniorTakeHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
