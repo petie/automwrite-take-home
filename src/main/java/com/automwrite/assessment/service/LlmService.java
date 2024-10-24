0166bf84d49497f8cfec304a0dc0ad692062ed69
7obyGit <7obyGitt@gmail.com>
2024-10-24 15:14:35 +0100

    Added empty LLM service files

---
+++ src/main/java/com/automwrite/assessment/service/LlmService.java
@@ -0,0 +1,5 @@
+package com.automwrite.assessment.service;
+
+public interface LlmService {
+    
+}
---
+++ src/main/java/com/automwrite/assessment/service/impl/LlmServiceImpl.java
@@ -0,0 +1,3 @@
+package com.automwrite.assessment.service.impl;
+
+public class LlmServiceImpl {}
